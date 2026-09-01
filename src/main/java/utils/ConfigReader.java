d8e7b84dfa1b82f6d5bbe2412e845fc54af5a7f8
vikrantgupta <vikrantgupta.qa@gmail.com>
2026-09-01 14:54:06 +0530

    Commit all new changes in base and page packages

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,5 +1,39 @@
 package utils;
 
-public class ConfigReader {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+public class ConfigReader {
+	private static final Properties props = new Properties();
+
+	static {
+		try (InputStream is = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+			if (is != null) {
+				props.load(is);
+			} else {
+				System.err.println("config.properties not found on classpath");
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to load config.properties", e);
+		}
+	}
+
+	public static String get(String key) {
+		return props.getProperty(key);
+	}
+
+	public static String get(String key, String defaultValue) {
+		return props.getProperty(key, defaultValue);
+	}
+
+	public static int getInt(String key, int defaultValue) {
+		String v = props.getProperty(key);
+		if (v == null) return defaultValue;
+		try {
+			return Integer.parseInt(v);
+		} catch (NumberFormatException e) {
+			return defaultValue;
+		}
+	}
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,10 +1,15 @@
 package tests;
 
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
+import base.BaseTest;
+import pages.LoginPage;
 
-public class LoginTest {
+public class LoginTest extends BaseTest {
+
 	@Test
 	public void testLogin() {
-	System.out.println("I am in login test");
+		System.out.println("I am in login test");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("testuser", "testpass");
 	}
 }
